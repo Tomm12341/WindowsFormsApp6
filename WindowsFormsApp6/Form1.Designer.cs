0b7a0e33a4cc38cf8fa5a699ce2d2b88cc8d4c82
aula <aula@INF3-PC04>
2024-02-02 11:50:55 +0100

    Aggiungere i file di progetto.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnPulisci = new System.Windows.Forms.Button();
+            this.btnSomma = new System.Windows.Forms.Button();
+            this.lblRisul = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "SOMMA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(321, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(272, 238);
+            this.listBox1.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(654, 28);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(165, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(664, 74);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(79, 38);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Aggiungi";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnPulisci
+            // 
+            this.btnPulisci.Location = new System.Drawing.Point(664, 128);
+            this.btnPulisci.Name = "btnPulisci";
+            this.btnPulisci.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnPulisci.Size = new System.Drawing.Size(79, 36);
+            this.btnPulisci.TabIndex = 7;
+            this.btnPulisci.Text = "Pulisci";
+            this.btnPulisci.UseVisualStyleBackColor = true;
+            // 
+            // btnSomma
+            // 
+            this.btnSomma.Location = new System.Drawing.Point(662, 181);
+            this.btnSomma.Name = "btnSomma";
+            this.btnSomma.Size = new System.Drawing.Size(81, 33);
+            this.btnSomma.TabIndex = 8;
+            this.btnSomma.Text = "Somma";
+            this.btnSomma.UseVisualStyleBackColor = true;
+            // 
+            // lblRisul
+            // 
+            this.lblRisul.AutoSize = true;
+            this.lblRisul.Location = new System.Drawing.Point(659, 253);
+            this.lblRisul.Name = "lblRisul";
+            this.lblRisul.Size = new System.Drawing.Size(48, 13);
+            this.lblRisul.TabIndex = 9;
+            this.lblRisul.Text = "Risultato";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 533);
+            this.Controls.Add(this.lblRisul);
+            this.Controls.Add(this.btnSomma);
+            this.Controls.Add(this.btnPulisci);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnPulisci;
+        private System.Windows.Forms.Button btnSomma;
+        private System.Windows.Forms.Label lblRisul;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fraction f1 = Fraction.Parse(textBox1.Text);
+             Fraction f2= Fraction.Parse(textBox2.Text);
+           
+            
+           textBox1.Text = f1.ToString();
+           textBox2.Text = f2.ToString();
+           
+            
+            Fraction risultato= f1.Somma(f2);
+            label1.Text=risultato.ToString();
+            
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
